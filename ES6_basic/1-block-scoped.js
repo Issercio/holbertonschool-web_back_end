4adaf46beccf2bba9fd07e4d6b3125c7e0e203b7
root <root@Didi>
2025-03-24 11:07:37 +0100

    add the 5 task

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,11 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    let taskInBlock = true;
+    let task2InBlock = false;
+
+    task = taskInBlock;
+    task2 = task2InBlock;
   }
 
   return [task, task2];

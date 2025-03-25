7de622361761980f43c65c8b6a5ea90d2347c7bc
Issercio <didi_1374@outlook.fr>
2025-03-25 10:12:37 +0100

    fix the task 5 and 9

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,23 +1,25 @@
-// 5-building.js
-
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new Error('Cannot instantiate an abstract class');
-    }
+    // Validate input is a number
     if (typeof sqft !== 'number') {
       throw new TypeError('sqft must be a number');
     }
+
+    // Store sqft with underscore prefix
     this._sqft = sqft;
+
+    // Ensure that this class cannot be directly instantiated if it's an abstract class
+    if (new.target === Building) {
+      throw new TypeError('Cannot instantiate abstract class Building directly');
+    }
   }
 
-  // Getter for 'sqft'
+  // Getter for sqft
   get sqft() {
     return this._sqft;
   }
 
-  // Abstract method to be implemented by subclasses
-  // eslint-disable-next-line class-methods-use-this
+  // Abstract method that must be implemented by subclasses
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }

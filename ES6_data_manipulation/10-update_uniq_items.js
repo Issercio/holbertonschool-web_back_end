bd05e2d39e70c9f8183cf5edf818c822b763d480
Issercio <didi_1374@outlook.fr>
2025-03-31 10:08:19 +0200

    add some files

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(groceries) {
+  if (!(groceries instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  groceries.forEach((quantity, item) => {
+    if (quantity === 1) {
+      groceries.set(item, 100);
+    }
+  });
+}
---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city) // Filter students by city
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A', // Assign grade or 'N/A' if not found
+      };
+    });
+}

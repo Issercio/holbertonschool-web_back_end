cb43fae33694c1bdf513c6162686db850f96a00f
Issercio <didi_1374@outlook.fr>
2025-04-01 09:52:31 +0200

    fix task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,14 +2,14 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ];
-
-  return Promise.allSettled(promises)
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+  const signUpPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+  
+  return Promise.allSettled([signUpPromise, uploadPromise])
+    .then((results) => {
+      return results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason
+      }));
+    });
 }

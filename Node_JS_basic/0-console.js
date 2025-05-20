b6bfd7d96f68d5d1a0a1394195753fec75082839
Issercio <didi_1374@outlook.fr>
2025-05-20 10:53:33 +0200

    fix the task 0

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,10 +1,3 @@
-'use strict';
-
-/**
- * Displays a message on the STDOUT
- * @param {string} message The message to display
- * @return {void}
- */
 function displayMessage(message) {
   console.log(message);
 }

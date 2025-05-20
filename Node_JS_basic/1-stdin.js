62493d9ec36b2e06bd2fdfd52467045a4451c584
Issercio <didi_1374@outlook.fr>
2025-05-20 11:10:12 +0200

    add fix the task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,5 +5,8 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
   const name = data.trim();
   process.stdout.write(`Your name is: ${name}\n`);
+});
+
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });

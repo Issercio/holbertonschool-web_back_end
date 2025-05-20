351fc8571aa4090257eddb204737a3d98dc86f20
Issercio <didi_1374@outlook.fr>
2025-05-20 13:18:04 +0200

    add the task 2 and 3

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,20 +1,9 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-// Create readable stream from stdin
-process.stdin.on('data', (data) => {
-  // Convert Buffer to string and remove trailing newline characters
-  const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-
-  // If input is coming from a pipe rather than terminal
-  if (!process.stdin.isTTY) {
-    process.stdout.write('This important software is now closing\n');
-    process.exit();
-  }
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) process.stdout.write(`Your name is: ${name}`);
 });
-
-// Handle end of input when piped
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
-  process.exit();
 });

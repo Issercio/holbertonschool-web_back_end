3e0544e5b08839a6f56f0cafd7b0855825461d9f
Issercio <didi_1374@outlook.fr>
2026-03-20 09:01:33 +0100

    add the project

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -2,8 +2,26 @@
 """Provide authentication helpers for password handling."""
 
 import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     """Return a salted bcrypt hash of the provided password string."""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str):
+        """Register a new user if email does not exist, else raise ValueError."""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_pwd = _hash_password(password)
+            return self._db.add_user(email, hashed_pwd)

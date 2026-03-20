acf6cbd21bb8c90fa3d38b3e6ef6d73f56eb4cfe
Issercio <didi_1374@outlook.fr>
2026-03-20 09:19:47 +0100

    add the project

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -13,22 +13,26 @@ def _hash_password(password: str) -> bytes:
 
 
 class Auth:
-        def valid_login(self, email: str, password: str) -> bool:
-            """
-            Validate user credentials.
-            Returns True if email exists and password matches, else False.
-            """
-            if not email or not password:
-                return False
-            try:
-                user = self._db.find_user_by(email=email)
-            except NoResultFound:
-                return False
-            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password.encode('utf-8'))
     """Auth class to interact with the authentication database."""
 
     def __init__(self):
         self._db = DB()
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Validate user credentials.
+        Returns True if email exists and password matches, else False.
+        """
+        if not email or not password:
+            return False
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(
+            password.encode('utf-8'),
+            user.hashed_password.encode('utf-8')
+        )
 
     def register_user(self, email: str, password: str) -> User:
         """
@@ -39,7 +43,12 @@ class Auth:
         """
         try:
             self._db.find_user_by(email=email)
-            raise ValueError(f"User {email} already exists")
+            raise ValueError(
+                f"User {email} already exists"
+            )
         except NoResultFound:
             hashed_pwd = _hash_password(password).decode('utf-8')
-            return self._db.add_user(email, hashed_pwd)
+            return self._db.add_user(
+                email,
+                hashed_pwd
+            )

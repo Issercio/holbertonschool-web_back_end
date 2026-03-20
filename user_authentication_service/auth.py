566400ab401b91fc5d4b94ab83a2f0bc05991337
Issercio <didi_1374@outlook.fr>
2026-03-20 09:53:31 +0100

    add the project

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Authentication service utilities."""
+
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """Return a salted hash of the input password using bcrypt."""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())

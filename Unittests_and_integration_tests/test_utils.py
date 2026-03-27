aaa90b67b1d6ad8c914fcd763497d73f428e9504
Issercio <didi_1374@outlook.fr>
2026-03-27 09:51:36 +0100

    add the project

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,14 +1,32 @@
 
-
 #!/usr/bin/env python3
 """Unit tests for utils and client modules."""
+import sys
+import os
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch, Mock
-import sys
-import os
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
+
+class TestMemoize(unittest.TestCase):
+    """Test case for the memoize decorator."""
+    def test_memoize(self):
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method", return_value=42) as mock_method:
+            obj = TestClass()
+            result1 = obj.a_property
+            result2 = obj.a_property
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mock_method.assert_called_once()
 
 
 class TestGetJson(unittest.TestCase):

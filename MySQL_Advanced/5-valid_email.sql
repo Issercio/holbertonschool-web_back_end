11291f0f78e15bc5b3192dee4efac792479b3398
Issercio <didi_1374@outlook.fr>
2026-04-03 11:52:08 +0200

    add the project

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,8 @@
+-- Trigger that resets valid_email only when the email has been changed
+CREATE TRIGGER reset_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+IF OLD.email != NEW.email THEN
+    SET NEW.valid_email = 0;
+END IF;
+ 

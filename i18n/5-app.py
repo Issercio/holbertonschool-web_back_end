3791b600d1706cb12f55cfbeb11919b7226dc177
Issercio <didi_1374@outlook.fr>
2026-04-03 09:54:23 +0200

    add the project

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -21,6 +21,7 @@ class Config:
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
@@ -45,6 +46,7 @@ def get_user():
 
     except (TypeError, ValueError):
         return None
+
 
 
 @app.before_request

052648c26efc175eb87f5515cee729321b59d52c
Issercio <didi_1374@outlook.fr>
2026-04-03 11:35:34 +0200

    add the project

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,7 +1,6 @@
--- 2. Best band ever!
--- Ranks country origins of bands by the number of (non-unique) fans
--- Rank country origins by total number of fans
 SELECT origin, SUM(nb_fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
+ORDER BY nb_fans DESC;
+ -- 2. Best band ever!
 ORDER BY nb_fans DESC;

aa140205aa343af47ef134fe9febf4708340807b
Issercio <didi_1374@outlook.fr>
2026-04-03 09:20:25 +0200

    add the project

---
+++ i18n/4-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Flask app with Babel, supporting forced locale via URL parameter.
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+_.__doc__ = """
+_ is an alias for the gettext translation function provided by Flask-Babel.
+It is used to mark strings for translation in the application and templates.
+"""
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel()
+
+def get_locale():
+    # Check for forced locale in URL parameter
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    # Fallback to best match from request headers
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+babel.init_app(app, locale_selector=get_locale)
+
+@app.route("/")
+def index():
+    return render_template("4-index.html", get_locale=get_locale)
+
+if __name__ == "__main__":
+    app.run()
